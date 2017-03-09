015072f06964b2bde10d4b76c0ea8ef59169269f
Hack Reactor Students <accounts@hackreactor.com>
2017-03-08 16:34:44 -0800

    Connected to server.

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -11,7 +11,7 @@ var Link = require('../app/models/link');
 // NOTE: these tests are designed for mongo!
 /////////////////////////////////////////////////////
 
-xdescribe('', function() {
+describe('', function() {
 
   beforeEach(function(done) {
     // Log out currently signed in user
@@ -125,6 +125,20 @@ xdescribe('', function() {
           .expect(function(res) {
             var redirect = res.headers.location;
             expect(redirect).to.equal('http://www.roflzoo.com/');
+          })
+          .end(done);
+      });
+      it('Visits should increase when link visited again', function(done) {
+        var sha = link.code;
+        var currVis = link.defaults.visits;
+        request(app)
+          .get('/' + sha)
+          .expect(function(res) {
+            Link.findOne({'url': 'http://www.roflzoo.com/'})
+              .exec(function(err, link) {
+                if (err) { console.log(err); }
+                expect(link.defaults.visits).to.equal(currVis + 1);
+              });
           })
           .end(done);
       });
